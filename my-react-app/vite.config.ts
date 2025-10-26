0aaa285422d634a5260417dac427cfb113c4d4aa
jding-3 <146374198+jding-3@users.noreply.github.com>
2025-10-25 20:50:29 -0700

    Add server configuration with proxy to vite.config.ts

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -4,8 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/generate-upload-url": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
+
